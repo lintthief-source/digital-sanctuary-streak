b99c63b55e1c437deae663614ec7e412bec767e9
lintthief-source <lintthief@gmail.com>
2025-12-31 20:19:02 -0700

    Update update-profile.js

--- api/update-profile.js
+++ api/update-profile.js
@@ -2,7 +2,21 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+  // 1. ADD THESE HEADERS FIRST
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*'); // For debugging, use '*' to allow everything
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // 2. Handle the "Pre-flight" OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
 
   const { customerId, firstName, lastName, nickname, dob, address, shop } = req.body;
 
